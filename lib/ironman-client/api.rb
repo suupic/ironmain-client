60031040e2013f7775858d90d95ca6ab52f1fe58
skywalker <skywalker418@gmail.com>
2012-04-24 12:50:11 +0800

    update

--- lib/ironman-client/api.rb
+++ lib/ironman-client/api.rb
@@ -11,6 +11,7 @@ module Ironman
       class << self
         def connect_to(server, port)
           @server = "http://#{server}:#{port}"
+
         end
 
 	def servers_by_product_id(product_id)
@@ -18,7 +19,44 @@ module Ironman
 	  res = get(url)
 	  if res[0]['code'] == -1
 	  else
-            res[0]['data'].collect { |data| "#{data['server_ip_address_internal']}" if !data['server_ip_address_internal'].empty? }.compact
+	    m = Hashie::Mash.new
+	    m.id = res[0]['data'][0]['product_id']
+	    m.name = res[0]['data'][0]['product_name']
+	    m.deploy_path = res[0]['data'][0]['product_deploy_path']
+	    m.server_ip_list = res[0]['data'].collect { |data| "#{data['server_ip_address_internal']}" if !data['server_ip_address_internal'].empty? }.compact
+	    return m
+	  end
+	end	
+
+	def servers_by_product_name(product_name)
+	  url = "/api/v1/products/name/#{product_name}"
+	  res = get(url)
+	  if res[0]['code'] == -1
+	  else
+	    m = Hashie::Mash.new
+	    m.id = res[0]['data'][0]['product_id']
+	    m.name = res[0]['data'][0]['product_name']
+	    m.deploy_path = res[0]['data'][0]['product_deploy_path']
+	    m.server_ip_list = res[0]['data'].collect { |data| "#{data['server_ip_address_internal']}" if !data['server_ip_address_internal'].empty? }.compact
+	    return m
+	  end
+	end	
+
+	def products
+	  url = "/api/v1/products"
+	  res = get(url)
+	  if res[0]['code'] == -1
+	  else
+            products = []
+	    res[0]['data'].each do |d|
+              m = Hashie::Mash.new
+	      m.id = d['id']
+	      m.name = d['name']
+	      m.package_name = d['package_name']
+	      m.deploy_path = d['deploy_path']
+	      products << m
+	    end
+	    return products
 	  end
 	end	
 
